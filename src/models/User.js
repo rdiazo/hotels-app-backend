cb076594326895ec48058863823e06829a66e310
Roberto D Diaz O <rddiazo75@gmail.com>
2024-04-02 14:22:58 -0400

    hotels app frontend v1.0.0.0

--- src/models/User.js
+++ src/models/User.js
@@ -20,7 +20,7 @@ const User = sequelize.define('user', {
         allowNull: false
     },
     gender: {
-        type: DataTypes.ENUM(['MALE', 'FEMALE', 'OTHER']),
+        type: DataTypes.ENUM(['Male', 'Female']),
         allowNull: false
     },
 });
